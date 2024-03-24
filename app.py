e95afe7bbbc9034de11c8b6eb6a508a796c12588
noerbarry <noerbarry@gmail.com>
2024-03-24 15:57:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def load_data():
     data = {
         'Usia': [25, 30, 28, 35, 23, 40, 22, 27],
         'Jenis_Kelamin': ['Laki-laki', 'Perempuan', 'Laki-laki', 'Perempuan', 'Laki-laki', 'Perempuan', 'Laki-laki', 'Perempuan'],
-        'Minat': ['Olahraga', 'Seni', 'Seni', 'Olahraga', 'Seni', 'Olahraga', 'Olahraga', 'Seni'],
+        'Minat': ['Olahraga', 'Seni', 'Seni', 'Olahraga', 'Seni', 'Olahraga', 'Olahraga', 'Seni','Musik'],
         'Nilai_Pribadi': ['Baik', 'Baik', 'Baik', 'Baik', 'Buruk', 'Buruk', 'Buruk', 'Baik'],
         'MBTI': ['INFJ', 'ENTP', 'INTJ', 'INTP', 'ENFJ', 'INFP', 'ENFP', 'INTJ'],
         'Twin_Flame': [1, 0, 1, 0, 1, 0, 1, 0]
